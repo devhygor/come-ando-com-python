746711ef68f06adcd63084ff774d435d8747eeab
devhygor <hygor.k92@gmail.com>
2022-07-30 17:07:56 -0300

    Atualizaçoes no projeto

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,24 +1,35 @@
+import random
+
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101) # função para gerar um numero aleatorio entre 1 e 100
+total_tentativas = 3
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou " , chute_str)
-chute = int(chute_str)
+for rodada in range (1, total_tentativas + 1):
+    print(f"Tentativa {rodada} de {total_tentativas}")
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
-if(acertou):
-    print("Parabéns! Você acertou!")
-else:
-    if(maior):
-        print("O seu chute foi maior do que o número secreto!")
-    elif(menor):
-        print("O seu chute foi menor do que o número secreto!")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+        break # se acertar ira parar o laço, finalizando o jogo
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
 
 print("Fim do jogo")
