51ef66a4279daaeef3bb0eafdada250dd1b87e3c
Hygor Rocha <hygor.k92@gmail.com>
2022-11-30 22:24:42 -0300

    continuação do exercicio da forca

--- forca.py
+++ forca.py
@@ -4,6 +4,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+    
+    index = 0
+    while(not enforcou and not acertou):
+        
+        chute = input('Qual letra? ')
+        chute = chute.strip()
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(f'Encontrei a letra {letra} na posição {index}')
+            index = index + 1
+        
+        print('jogando ...')
 
     print("Fim de Jogo")
 
