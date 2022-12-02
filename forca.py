de43b14c7c160ebb47604d487e626cb75a664c47
Hygor Rocha <hygor.k92@gmail.com>
2022-12-01 22:21:58 -0300

    finalizando exercicio da forca

--- forca.py
+++ forca.py
@@ -1,28 +1,52 @@
+import random
 
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    
+    arquivo.close()
+    
+    posicao = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[posicao].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta] 
     
     enforcou = False
     acertou = False
+    erros = 0
+    
+    print(letras_acertadas)
     
     index = 0
     while(not enforcou and not acertou):
         
         chute = input('Qual letra? ')
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print(f'Encontrei a letra {letra} na posição {index}')
-            index = index + 1
-        
-        print('jogando ...')
-
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    if (acertou):
+        print("Você Ganhou!!")
+    else:
+        print("Game Over")
     print("Fim de Jogo")
 
 if (__name__=="__main__"):
